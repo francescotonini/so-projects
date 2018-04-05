1227c56fe24e3ed3fcdb09b4f2d60a0aa8e44477
Francesco Tonini <francescoantoniotonini@gmail.com>
2018-04-05 10:27:20 +0200

    Check user existence and fix grep match

--- project1/query.bash
+++ project1/query.bash
@@ -4,18 +4,25 @@
 cd reports/
 
 # 1.
-# Since reports aren't modified once they have been created, we can sort by last modification
-# time
+# Sorts by last modification. Because file doesn't change after creation,
+# we can assume that last modification date equals creation date
 lastReport=$(ls -t report_*.csv | head -n1)
 echo Last report: $lastReport
 while read user; do
-	echo $user: $(cat $lastReport | cut -d ";" -f1 | grep $user | wc -l)
+	echo $user: $(cat $lastReport | cut -d ";" -f1 | grep "\b$user\b" | wc -l)
 done <<< "$(cat $lastReport | cut -d ";" -f1 | sort -u)"
 
 # 2.
 read -p "Inserire utente: " user
-for file in $(ls -t report_*.csv); do
-	echo report: $file processi $(cat $file | cut -d ";" -f1 | grep $user | wc -l)
-done
+
+# Check if user exists
+id -u $user >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	for file in $(ls -t report_*.csv); do
+		echo report: $file processi $(cat $file | cut -d ";" -f1 | grep "\b$user\b" | wc -l)
+	done
+else
+	echo $user non esiste
+fi
 
 exit 0
