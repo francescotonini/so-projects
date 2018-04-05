289ef634483fe673c5b48737b2010b185f03591a
Francesco Tonini <francescoantoniotonini@gmail.com>
2018-04-05 22:33:02 +0200

    Language update

--- project1/checkSpace.bash
+++ project1/checkSpace.bash
@@ -4,12 +4,12 @@ THRESHOLD=64 # kbytes
 cd reports
 
 if [ $(du -c report_*.csv | tail -n1 | cut -f1) -le $THRESHOLD ]; then
-	echo "Niente da cancellare..."
+	echo Niente da cancellare...
 else
 	while [ $(du -c report_*.csv | tail -n1 | cut -f1) -gt $THRESHOLD ]; do
 		# echo Current: $(du -c report_*.csv | tail -n1 | cut -f1) threshold: $THRESHOLD
 		lastFile=$(ls -tr report_*.csv | head -n1)
-		echo $lastFile deleted
+		echo $lastFile rimosso
 		rm $lastFile
 	done;
 fi
