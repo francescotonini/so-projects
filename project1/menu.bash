ad5fbae595c8f5efec210f23c694e551a449fdbc
Francesco Tonini <francescoantoniotonini@gmail.com>
2018-04-05 22:33:12 +0200

    Fix recorder launch

--- project1/menu.bash
+++ project1/menu.bash
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-function menu {
+function startMonitor {
+	pid=$(pgrep -f "bash recorder.bash")
+	if [ $? -gt 0 ]; then
+		# Launch recorder on a new session
+		setsid bash recorder.bash >/dev/null 2>&1 &
+		echo Monitoraggio avviato: $!
+	else
+		echo Monitoraggio già in esecuzione
+	fi
+}
+
+function stopMonitor {
+	pid=$(pgrep -f "bash recorder.bash")
+	if [ $? -eq 0 ]; then
+		kill -TERM -$pid
+		wait $pid 2>/dev/null
+		echo Monitoraggio terminato: $pid
+	else
+		echo Monitoraggio non in esecuzione
+	fi
+}
+
+while true; do
 	echo "Monitoraggio server. Comandi disponibili:"
 	echo "1) Inizia monitoraggio"
 	echo "2) Ferma monitoraggio"
@@ -10,30 +32,10 @@ function menu {
 
 	read selection
 	case $selection in
-		1) startMonitor && menu;;
-		2) stopMonitor && menu;;
+		1) startMonitor;;
+		2) stopMonitor;;
 		3) bash query.bash;;
 		4) exit 0;;
-		5) bash checkSpace.bash && menu;;
-		*) menu;;
+		5) bash checkSpace.bash;;
 	esac
-}
-
-function startMonitor {
-	pid=$(pgrep -f "bash recorder.bash")
-	if [ $? -gt 0 ]; then
-		bash recorder.bash >/dev/null 2>&1 &
-		echo "Monitoraggio avviato"
-	fi
-}
-
-function stopMonitor {
-	pid=$(pgrep -f "bash recorder.bash")
-	if [ $? -eq 0 ]; then
-		kill -TERM $pid
-		wait $pid 2>/dev/null
-		echo "Monitoraggio terminato ($pid)"
-	fi
-}
-
-menu
+done;
