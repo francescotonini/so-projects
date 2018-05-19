75e73deb040c80f776e6de6ea831a3f61f0de2c7
Francesco <francescoantoniotonini@gmail.com>
2018-05-19 08:56:56 +0200

    Add error handler£

---
+++ project2/include/error_handler.h
@@ -0,0 +1,14 @@
+/// @file
+
+/// @defgroup error_handler
+/// @{
+
+#ifndef ERROR_HANDLER
+#define ERROR_HANDLER
+/**
+ * @brief funzione di errore alternativa
+ */
+void syserr (char *prog, char *msg);
+#endif
+
+/// @}
